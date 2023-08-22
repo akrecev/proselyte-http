aea779cf6adc9a6062dea8100dd196d958b339d3
RWEC\akrecev <akretsev@gmail.com>
2023-08-22 14:31:28 +0300

    feat: add test servlets

---
+++ src/main/java/com/kretsev/servlet/StudyServlet.java
@@ -0,0 +1,27 @@
+package com.kretsev.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class StudyServlet extends HttpServlet {
+    private String message;
+
+    public void init() throws ServletException {
+        message = "This is StudyServlet message";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        PrintWriter messageWriter = response.getWriter();
+        messageWriter.println("<h1>" + message + "</h1>");
+    }
+
+    public void destroy() {
+
+    }
+}
