b799628730257aabfa4ebb19e84f5d47d016335b
RWEC\akrecev <akretsev@gmail.com>
2023-08-31 10:23:33 +0300

    feat: add CreateFile servlet

---
+++ src/main/java/com/kretsev/servlet/file/CreateFile.java
@@ -0,0 +1,59 @@
+package com.kretsev.servlet.file;
+
+import com.kretsev.model.File;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static com.kretsev.context.ApplicationContext.getFileService;
+
+public class CreateFile extends HttpServlet {
+    private final static String CONTENT_TYPE = "text/html";
+    private final static String DOC_TYPE = "<!DOCTYPE html>";
+    private final static String TITLE = "Create File";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType(CONTENT_TYPE);
+        PrintWriter writer = resp.getWriter();
+
+        writer.println(DOC_TYPE + "\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "<title>" + TITLE + "</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "Method GET not supported" +
+                "\n</body>" +
+                "\n</html>");
+
+        resp.setStatus(resp.SC_METHOD_NOT_ALLOWED);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType(CONTENT_TYPE);
+        PrintWriter writer = resp.getWriter();
+
+        String fileName = req.getParameter("name");
+        String filePath = req.getParameter("path");
+        File file = File.builder().name(fileName).filePath(filePath).build();
+        file = getFileService().create(file);
+
+        writer.println(DOC_TYPE + "\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "<title>" + TITLE + "</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "Successfully creating file:\n" + file.toString() +
+                "\n</body>" +
+                "\n</html>");
+
+        resp.setStatus(resp.SC_OK);
+    }
+}
