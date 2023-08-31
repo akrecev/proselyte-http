e6034c124335836cc767ce5893c5745a5c5931a1
RWEC\akrecev <akretsev@gmail.com>
2023-08-31 10:25:54 +0300

    feat: add UpdateFile servlet

---
+++ src/main/java/com/kretsev/servlet/file/UpdateFile.java
@@ -0,0 +1,65 @@
+package com.kretsev.servlet.file;
+
+import com.kretsev.model.File;
+import com.kretsev.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static com.kretsev.context.ApplicationContext.getFileService;
+import static com.kretsev.context.ApplicationContext.getUserService;
+
+public class UpdateFile extends HttpServlet {
+    private final static String CONTENT_TYPE = "text/html";
+    private final static String DOC_TYPE = "<!DOCTYPE html>";
+    private final static String TITLE = "Update File";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType(CONTENT_TYPE);
+        PrintWriter writer = resp.getWriter();
+
+        writer.println(DOC_TYPE + "\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "<title>" + TITLE + "</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "Method GET not supported" +
+                "\n</body>" +
+                "\n</html>");
+
+        resp.setStatus(resp.SC_METHOD_NOT_ALLOWED);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType(CONTENT_TYPE);
+        PrintWriter writer = resp.getWriter();
+
+        Integer fileId = Integer.parseInt(req.getParameter("id")) ;
+        String fileName = req.getParameter("name");
+        String filePath = req.getParameter("path");
+        File file = File.builder().id(fileId).name(fileName).filePath(filePath).build();
+        file = getFileService().update(file);
+        file = getFileService().getById(file.getId());
+        //TODO при вызове .update без .getById не подгружается список ивентов
+        // (session.merge(file) в HibernateFileRepositoryImpl не загружает вложенные сущности из БД)
+
+        writer.println(DOC_TYPE + "\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "<title>" + TITLE + "</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "Successfully updating file:\n" + file.toString() +
+                "\n</body>" +
+                "\n</html>");
+
+        resp.setStatus(resp.SC_OK);
+    }
+}
